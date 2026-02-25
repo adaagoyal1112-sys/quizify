9dd6af1a2e9e6867c35f157b2cd2556714ce4e4c
Adaa Goyal <adaagoyal1112@gmail.com>
2026-02-25 08:02:52 +0530

    Fix App duplicate and use HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Landing from './pages/Landing'
-import  Quiz from './pages/Quiz'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Homepage from './pages/Homepage'
-import ScoreList from "./pages/ScoreList"
-  const router = createBrowserRouter([
-    {
-      path:"/",
-      element: <Landing />
-    },
-    {
-      path: "/quiz",
-      element: <Quiz />
-    },
-    {
-      path:"/login",
-      element:<Login />
-    },
-    {
-      path:"/register",
-      element:<Register />
-    },
-    {
-      path:"/scorelist",
-      element:<ScoreList />
-    },
-    {
-      path:"/homepage",
-      element:<Homepage />
-    }
-  ])
+import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import Quiz from "./pages/Quiz";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ScoreList from "./pages/ScoreList";
+import Homepage from "./pages/Homepage";
 
 function App() {
-
   return (
-    <>
-    
-    <RouterProvider  router={router}/>
-    </>
-  )
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/scorelist" element={<ScoreList />} />
+        <Route path="/homepage" element={<Homepage />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
